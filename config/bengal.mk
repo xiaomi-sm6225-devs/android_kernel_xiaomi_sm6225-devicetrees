f78a82703a6c07c2d220d81d9425a033a68c0f9b
Nirmal Abraham <c_nabrah@qti.qualcomm.com>
2022-08-30 16:43:05 +0530

    ARM: dts: msm: Rename khaje.mk to bengal.mk
    MSM_ARCH variable name was changed from khaje to bengal,
    so to pick the config file correctly in build and generate
    camera dtbo, rename khaje.mk to bengal.mk.
    
    Change-Id: I98021de8a936d5e3a69f8fed6a15363025959dc9

--- config/bengal.mk
+++ config/bengal.mk
@@ -1,3 +1,3 @@
 dtbo-$(CONFIG_ARCH_KHAJE) := khaje-camera.dtbo
 dtbo-$(CONFIG_ARCH_KHAJE) += khaje-camera-sensor-idp.dtbo \
-								khaje-camera-sensor-qrd.dtbo \
+				khaje-camera-sensor-qrd.dtbo \
