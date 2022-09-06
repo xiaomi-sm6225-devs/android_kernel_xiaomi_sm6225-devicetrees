1a85726dded01d2b46935efe0ddf9035db0eada7
Nirmal Abraham <c_nabrah@qti.qualcomm.com>
2022-09-06 06:22:51 +0530

    ARM: dts: msm: Changes to enable camera on khaje QRD
    Changes to enable camera on QRD HVDCP3P5.
    
    Change-Id: I64fb475b1167deff367f17cb28a00f9576210a51

--- config/bengal.mk
+++ config/bengal.mk
@@ -3,3 +3,4 @@ dtbo-$(CONFIG_ARCH_KHAJE) += khaje-camera-sensor-idp.dtbo \
 				khaje-camera-sensor-qrd.dtbo \
 				khaje-camera-sensor-idp-nopmi.dtbo \
 				khaje-camera-sensor-qrd-nopmi.dtbo \
+				khaje-camera-sensor-qrd-hvdcp3p5.dtbo \
