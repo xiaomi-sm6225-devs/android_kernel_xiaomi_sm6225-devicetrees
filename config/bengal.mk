fb7455795b7d95d8e4db368d8daa7057411d1064
Nirmal Abraham <c_nabrah@qti.qualcomm.com>
2022-09-12 16:39:48 +0530

    ARM: dts: msm: Enable camera on khaje nopmi device
    Changes to enable camera on nopmi varient of khaje.
    
    Change-Id: I2f061c41ee48394828c9db261a327654455cd202

--- config/bengal.mk
+++ config/bengal.mk
@@ -1,3 +1,5 @@
 dtbo-$(CONFIG_ARCH_KHAJE) := khaje-camera.dtbo
 dtbo-$(CONFIG_ARCH_KHAJE) += khaje-camera-sensor-idp.dtbo \
 				khaje-camera-sensor-qrd.dtbo \
+				khaje-camera-sensor-idp-nopmi.dtbo \
+				khaje-camera-sensor-qrd-nopmi.dtbo \
