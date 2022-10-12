cb2c5b22122a961a169d0eae41a778465cd569d4
Nirmal Abraham <c_nabrah@qti.qualcomm.com>
2022-10-12 11:55:01 +0530

    ARM: dts: msm: disable camera for khaje gaming variant
    Camera is not supported in gaming device, so disable
    the camera nodes for gaming hw variant.
    
    Change-Id: I219c1a611727cdda96be91ccbc0ac168213ed534

--- config/bengal.mk
+++ config/bengal.mk
@@ -1,5 +1,8 @@
 dtbo-$(CONFIG_ARCH_KHAJE) := khaje-camera.dtbo
-dtbo-$(CONFIG_ARCH_KHAJE) += khaje-camera-sensor-idp.dtbo \
+dtbo-$(CONFIG_ARCH_KHAJE) += khajeg-camera-idp.dtbo \
+				khajeg-camera-qrd.dtbo \
+				khajeg-camera-qrd-hvdcp3p5.dtbo \
+				khaje-camera-sensor-idp.dtbo \
 				khaje-camera-sensor-qrd.dtbo \
 				khaje-camera-sensor-idp-nopmi.dtbo \
 				khaje-camera-sensor-qrd-nopmi.dtbo \
