ad9e341757d0d5e7e19b82f95f62b4f100bcbca4
Nirmal Abraham <c_nabrah@qti.qualcomm.com>
2022-10-18 09:11:52 +0530

    ARM: dts: msm: Enable camera for IDPS and other variants
    Enable camera for IDPS(CDP), idp-usbc and
    qrd-nowcd9375 devices.
    
    Change-Id: Ib920ade7ae4d046914993aad5c01ebf90e7934b5

--- config/bengal.mk
+++ config/bengal.mk
@@ -7,3 +7,6 @@ dtbo-$(CONFIG_ARCH_KHAJE) += khajeg-camera-idp.dtbo \
 				khaje-camera-sensor-idp-nopmi.dtbo \
 				khaje-camera-sensor-qrd-nopmi.dtbo \
 				khaje-camera-sensor-qrd-hvdcp3p5.dtbo \
+				khaje-camera-sensor-qrd-nowcd9375.dtbo \
+				khaje-camera-sensor-idps-display-90hz.dtbo \
+				khaje-camera-sensor-idp-usbc.dtbo \
